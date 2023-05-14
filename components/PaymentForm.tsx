71797a998899d449dc28d3619bbb3347fb3ebb99
unknown <yury.ovchinnkov@yandex.ru>
2023-05-14 16:48:32 +0300

    add inputs

--- components/PaymentForm.tsx
+++ components/PaymentForm.tsx
@@ -1,7 +1,10 @@
-import React, { ChangeEvent, ReactNode, useState } from "react";
+import React, { ChangeEvent, ReactNode, useContext, useState } from "react";
 import Cards, { Focused } from "react-credit-cards-2";
 import "node_modules/react-credit-cards-2/dist/es/styles-compiled.css";
-import { TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
+import { addDoc, collection, setDoc } from "firebase/firestore";
+import { Context } from "@/pages/_app";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const PaymentForm = () => {
   const [state, setState] = useState({
@@ -10,7 +13,27 @@ const PaymentForm = () => {
     cvc: "",
     name: "",
     focus: "",
+    country: "",
+    city: "",
+    street: "",
+    house: "",
+    floor: "",
+    apartment: "",
   });
+
+  const {
+    apartment,
+    city,
+    country,
+    cvc,
+    expiry,
+    floor,
+    focus,
+    house,
+    name,
+    number,
+    street,
+  } = state;
 
   const handleInputChange = (
     event: ChangeEvent<{
@@ -25,15 +48,67 @@ const PaymentForm = () => {
     setState((prev) => ({ ...prev, [name as string]: value as string }));
   };
 
+  const Pay = () => {};
+
   return (
     <div className="payment">
+      <form className="payment__inputs">
+        <TextField
+          type="text"
+          variant="outlined"
+          name="country"
+          className="payment__inputs-input"
+          label="Country"
+          onChange={handleInputChange as VoidFunction}
+        />
+        <TextField
+          type="text"
+          variant="outlined"
+          name="city"
+          className="payment__inputs-input"
+          label="City"
+          onChange={handleInputChange as VoidFunction}
+        />
+        <TextField
+          type="text"
+          variant="outlined"
+          name="street"
+          className="payment__inputs-input"
+          label="Street"
+          onChange={handleInputChange as VoidFunction}
+        />
+        <TextField
+          type="text"
+          variant="outlined"
+          name="house"
+          className="payment__inputs-input"
+          label="House"
+          onChange={handleInputChange as VoidFunction}
+        />
+        <TextField
+          type="text"
+          variant="outlined"
+          name="floor"
+          className="payment__inputs-input"
+          label="Floor"
+          onChange={handleInputChange as VoidFunction}
+        />
+        <TextField
+          type="text"
+          variant="outlined"
+          name="apartment"
+          className="payment__inputs-input"
+          label="Apartment"
+          onChange={handleInputChange as VoidFunction}
+        />
+      </form>
       <div className="payment__card">
         <Cards
-          number={state.number}
-          expiry={state.expiry}
-          cvc={state.cvc}
-          name={state.name}
-          focused={state.focus as Focused}
+          number={number}
+          expiry={expiry}
+          cvc={cvc}
+          name={name}
+          focused={focus as Focused}
         />
       </div>
       <form className="payment__inputs">
@@ -62,13 +137,14 @@ const PaymentForm = () => {
         />
         <TextField
           variant="outlined"
-          type="number"
+          type="numberzz"
           name="number"
           className="payment__inputs-input"
           label="Card Number"
           onChange={handleInputChange as VoidFunction}
         />
       </form>
+      <Button onClick={Pay}>Оплатить</Button>
     </div>
   );
 };
